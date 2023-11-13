ecdec83afc5a028fd3c8b9f5bb41752565a86567
Chris Woodruff <cwoodruff@live.com>
2023-11-13 04:55:15 -0500

    added the output caching code

---
+++ output-caching/Chinook.API/Program.cs
@@ -0,0 +1,29 @@
+using Chinook.API.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAppSettings(builder.Configuration);
+builder.Services.AddConnectionProvider(builder.Configuration);
+builder.Services.ConfigureRepositories();
+builder.Services.ConfigureSupervisor();
+builder.Services.AddAPILogging();
+builder.Services.AddCORS();
+builder.Services.ConfigureValidators();
+builder.Services.AddCaching();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseOutputCache();
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
